6eb68292891d7995e13c1dbf432c773be8236927
Santiago Galindo <sp.galindoh@gmail.com>
2020-08-19 22:04:15 -0500

    first commit

---
+++ packages/JTableRow/JTableRow.stories.js
@@ -0,0 +1,14 @@
+import { storiesOf } from "@storybook/vue";
+import JTableRow from "./JTableRow";
+
+storiesOf("JTableRow", module).add("normal", () => ({
+  components: { JTableRow },
+  template: `
+      <JTableRow
+        :values="values"
+      />
+    `,
+  data: () => ({
+    values: ["Santiago Galindo", "sp.galindoh@gmail.com"]
+  })
+}));
